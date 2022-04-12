93880de36f203078d3ebec6a266b3046a7989cf0
M4ti5 <Caffiauxmatis@gmail.com>
2022-04-13 01:30:05 +0200

    Tensorflow plugins

---
+++ VR 5.0/Content/Scripts/CNN_API.py
@@ -0,0 +1,31 @@
+from unittest import result
+import tensorflow as tf
+import unreal_engine as ue
+from TFPluginAPI import TFPPluginAPI
+
+from numpy import reshape
+
+class CNN_Hirigana(TFPPluginAPI):
+    def onSetup(self):
+        self.model = tf.keras.models.load.model("Hiragana_CNN_v1")
+        self.sess = tf.InteracticeSession()
+        self.graph = tf.get_default_graph()
+        ue.log("-----OnSetup-----")
+    
+    def onJsonInput (self, jsonInput):
+        ue.log("-----Recive Input-----")
+        ue.log("-----Reshape Input - START-----")
+        x = jsonInput['pixel']
+        x = reshape(x , (1,1024,1024))
+        x =  tf.keras.preprocessing.image.smart_resize(x, (28,28), interpolation='bilinear')
+        
+        
+        with self.graph.as_default():
+            ue.log("-----Prediction - START-----")
+            output = self.model.predict(x)
+            index , value = output.index(max(output)), max(output)
+            result["prediction"] = index
+            ue.log("-----Prediction - END-----")
+    
+    def getApi():
+        return CNN_Hirigana.getInstance()
---
+++ VR 5.0/Intermediate/Build/Win64/UE4_VRPlaygroundEditor/Development/UnrealEd/SharedPCH.UnrealEd.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30137
+#include "C:/Users/Matis_CAFFIAUX/Documents/Dev_Lab/Unreal/VRgame/VR 5.0/Intermediate/Build/Win64/UE4_VRPlaygroundEditor/Development/UnrealEd/SharedPCH.UnrealEd.h"
